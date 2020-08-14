0723cc2303b706f84c76dd7e27bef82d2de213cb
Dominic Jesse <dominic.jesse@morningstar.com>
2020-08-14 17:07:25 -0500

    move adventure text to browser

--- advGameEngine/adv.js
+++ advGameEngine/adv.js
@@ -2,8 +2,7 @@
 
 import { Location, Exit, Item } from './advUtilities.js';
 
-
-//setup games
+//setup game
 var roomObjects = [], itemObjects = [], squareSide = 8;
 
 var roomRow = 0, roomRowArray = [];
@@ -21,19 +20,6 @@ for (var j = 0; j < gameItems.length; j++) {
   itemObjects.push(new Item(gameItems[j]));
 }
 
-/*
-2. Print description of location and exits
-3. Print message or additional info from player instruction
-4. Ask player for next instruction
-5. Check instructions against known list of words and verbs
-6a. If instructions don't make sense, reprompt with error message and wait
-   for another instruction
-6b. If instruction makes sense, use appropriate function to take appropriate action
-7. Use function to take action as instructed
-8. Goto 2
-
-*/
-
 //console.log(roomObjects[2][1]); //back of hallway
 //console.log(roomObjects[7][0]); //twisted railings
 //console.log(roomObjects[3][6]); //cellar
@@ -44,21 +30,29 @@ var currentRow = 0, currentCol = 0;
 
 while (playing) {
   var currentRoom = roomObjects[currentRow][currentCol];
-  var currentRoomName = currentRoom.name;
-  console.log("\n YOUR LOCATION:\n");
-  console.log(currentRoomName + "\n");
-  console.log("\nEXITS:\n");
-  for (var i = 0; i < currentRoom.exits.length; i++)
-    console.log(currentRoom.exits[i].displayDirection + "\n")
-  for (var j = 0; j < itemObjects.length; j++) {
-    var seeItems = false;
-    if (itemObjects[j].currentLocation === currentRoom) {
-      if (!seeItems)
-        console.log("YOU SEE: \n");
-      seeItems = true;
-      console.log(itemObjects[j].name);
-    }
-  }
-  console.log("?: ");
+  showLocation(currentRoom);
+  showExits(currentRoom);
+  showRoomItems(currentRoom);
   break;
+}
+
+function showLocation(room) {
+  document.getElementById("locationDesc").innerHTML = room.name;
+}
+
+function showExits(room) {
+  var exitHTML = "";
+  for (var i = 0; i < room.exits.length; i++)
+    exitHTML += currentRoom.exits[i].displayDirection + "</br>";
+  document.getElementById("exitDesc").innerHTML = exitHTML;
+}
+
+function showRoomItems(room) {
+  var itemHTML = "";
+  for (var j = 0; j < itemObjects.length; j++)
+    if (itemObjects[j].currentLocation === room.name)
+      itemHTML += itemObjects[j].name + "</br>";
+  if (itemHTML.length == 0)
+    itemHTML = "NOTHING</br>";
+  document.getElementById("itemDesc").innerHTML = itemHTML;
 }
